2a4c1805b36b6f9a941c1513d7167ee3b74c4207
askmebefore <zayats1812@mail.ru>
2022-08-23 13:38:40 +0300

    rewrote markers (the old way) added pct calculation for all genes in the dataset

--- workflow/rules/analysis/seurat_analysis.smk
+++ workflow/rules/analysis/seurat_analysis.smk
@@ -57,12 +57,14 @@ rule markers_default:
         marker_dir = rules.markers.output.marker_dir
     output:
         markers = config["out_dir"] + "/data/samples/{dataset}/{sample}/markers.tsv",
-        clusters = config["out_dir"] + "/data/samples/{dataset}/{sample}/clusters.tsv"
+        clusters = config["out_dir"] + "/data/samples/{dataset}/{sample}/clusters.tsv",
+        pct = config["out_dir"] + "/data/samples/{dataset}/{sample}/pct.tsv",
     params:
         default_resolution = DEFAULT_RESOLUTION
     log: config['logs_dir'] + "/{dataset}/{sample}/markers_default.log"
     benchmark: config['logs_dir'] + "/{dataset}/{sample}/markers_default.benchmark"
     shell: """
+    cp "{input.marker_dir}/clusters_{params.default_resolution}_pct.tsv" {output.pct}
     cp "{input.marker_dir}/clusters_{params.default_resolution}_average.tsv" {output.clusters}
     cp "{input.marker_dir}/markers_{params.default_resolution}.tsv" {output.markers}
     echo `date`> {log}
--- workflow/scripts/markers.R
+++ workflow/scripts/markers.R
@@ -5,31 +5,71 @@ data <- readRDS(snakemake@input$seurat)
 outputMarkerDir <- snakemake@output$marker_dir
 dir.create(outputMarkerDir, recursive = T)
 
+
+DefaultAssay(data) <- "RNA"
+data <- NormalizeData(data)
+
+averagePCT <- function(seurat,
+                       ident, # columns from @meta.data
+                   assay="RNA",
+                       slot="counts",
+                       threshold=0) {
+  seurat <- SetIdent(seurat, value=ident)
+  allLevels <- levels(Idents(seurat))
+  data <- GetAssayData(seurat, assay=assay, slot=slot)
+
+  results <- matrix(nrow=nrow(data), ncol=length(allLevels),
+                    dimnames = list(rownames(data), allLevels))
+
+  for (i in 1:length(allLevels)) {
+    ident <- allLevels[i]
+    cell.ids <- which(Idents(seurat) == ident)
+    results[, i] <- round(rowSums(data[, cell.ids, drop=F] > threshold) / length(cell.ids), digits=3)
+  }
+  return(results)
+}
+
+averageExpression <- function(seurat,
+                              ident,
+                              assay="RNA",
+                              slot="data") {
+  cluster.averages <- AverageExpression(object = seurat, group.by=ident, assays = assay, slot = slot)
+  return(cluster.averages[[1]])
+}
+
+allMarkers <- function(seurat,
+                       ident,
+                       assay="RNA",
+                       slot="data") {
+
+  Idents(seurat) <- seurat[[ident]]
+  whole.markers <- FindAllMarkers(object = seurat,
+                                  assay=assay,
+                                  slot=slot,
+                                  only.pos = TRUE,
+                                  min.pct = 0.10,
+                                  test.use = 'wilcox',
+                                  max.cells.per.ident = 3e3,
+                                  random.seed = 42)
+  return(whole.markers)
+}
+
+
+
 for (resolution in snakemake@params$resolutions) {
-  
+
   identName <- paste0('SCT_snn_res.', resolution)
-  Idents(data) <- data[[identName]]
-  cluster.averages <- AverageExpression(object = data, assays = 'SCT', slot = 'data')[[1]]
-  write.table(cluster.averages, 
-              file=file.path(outputMarkerDir, paste0("clusters_", resolution, "_average.tsv")))
-  
-  deResults <- FindAllMarkers(object = data,
-                              assay='SCT',
-                              slot='data',
-                              
-                              only.pos = FALSE,
-                              
-                              min.pct = 0,
-                              logfc.threshold = 0,
-                              
-                              max.cells.per.ident = 20,
-                              return.thresh=1.01,
-                              
-                              test.use = 'wilcox')
-  
-  write.table(deResults, 
+
+  clusterAverages <- averageExpression(data, identName)
+  write.table(clusterAverages, file=file.path(outputMarkerDir, paste0("clusters_", resolution, "_average.tsv")))
+
+  clusterPCTs <- averagePCT(data, identName)
+  write.table(clusterPCTs, file=file.path(outputMarkerDir, paste0("clusters_", resolution, "_pct.tsv")))
+
+  deResults <- allMarkers(data, identName)
+  write.table(deResults,
               file.path(outputMarkerDir, paste0("markers_", resolution, ".tsv")),
-              sep="\t", 
+              sep="\t",
               quote=F,
               row.names=F)
   
